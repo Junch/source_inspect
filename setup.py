14df9ea68bff67898e382d3e2da35dbf88949b06
chenj-bd <chenj-bd@glodon.com>
2023-11-07 21:09:24 +0800

    Update source for Inspect

--- setup.py
+++ setup.py
@@ -126,13 +126,13 @@ class CMakeBuild(build_ext):
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="cmake_example",
+    name="source_inspect",
     version="0.0.1",
     author="Dean Moldovan",
-    author_email="dean0x7d@gmail.com",
-    description="A test project using pybind11 and CMake",
+    author_email="junc76@gmail.com",
+    description="Query source line from the symbol name",
     long_description="",
-    ext_modules=[CMakeExtension("cmake_example")],
+    ext_modules=[CMakeExtension("source_inspect")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,89 @@
 #include <pybind11/pybind11.h>
+#include <windows.h>
+#include <string>
+#include <dbghelp.h>
+#include <sstream>
+#pragma comment(lib,"dbghelp.lib")
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
-
 namespace py = pybind11;
 
-PYBIND11_MODULE(cmake_example, m) {
+struct SymLineInfo
+{
+    SymLineInfo() : LineNumber(0) {}
+    std::wstring FileName;
+    DWORD        LineNumber;
+};
+
+class Inspect
+{
+public:
+    Inspect(DWORD pid);
+    ~Inspect();
+
+    SymLineInfo GetLineFromSymName(PCWSTR pFunctionName);
+private:
+    HANDLE m_hProcess;
+    BOOL m_bInitialzed;
+};
+
+Inspect::Inspect(DWORD pid)
+{
+    m_hProcess = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
+    m_bInitialzed = SymInitialize(m_hProcess, NULL, TRUE);
+}
+
+Inspect::~Inspect()
+{
+    if (m_bInitialzed)
+    {
+        SymCleanup(m_hProcess);
+    }
+
+    CloseHandle(m_hProcess);
+}
+
+SymLineInfo Inspect::GetLineFromSymName(PCWSTR pFunctionName)
+{
+    SymLineInfo ret;
+
+    TCHAR szSymbolName[MAX_SYM_NAME];
+    ULONG64 buffer[(sizeof(SYMBOL_INFOW) +
+        MAX_SYM_NAME * sizeof(TCHAR) +
+        sizeof(ULONG64) - 1) /
+        sizeof(ULONG64)];
+    PSYMBOL_INFOW pSymbol = (PSYMBOL_INFOW)buffer;
+
+    wcscpy_s(szSymbolName, MAX_SYM_NAME, pFunctionName);
+    pSymbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+    pSymbol->MaxNameLen = MAX_SYM_NAME;
+
+    if (!SymFromNameW(m_hProcess, pFunctionName, pSymbol))
+    {
+        return ret;
+    }
+
+    DWORD  dwDisplacement;
+    IMAGEHLP_LINEW64 line;
+    line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+    DWORD64 dwAddress = pSymbol->Address;
+    if (SymGetLineFromAddrW64(m_hProcess, dwAddress, &dwDisplacement, &line))
+    {
+        ret.FileName = line.FileName;
+        ret.LineNumber = line.LineNumber;
+    }
+
+    return ret;
+}
+
+PYBIND11_MODULE(source_inspect, m) {
     m.doc() = R"pbdoc(
-        Pybind11 example plugin
+        Pybind11 source_inspect plugin
         -----------------------
 
-        .. currentmodule:: cmake_example
+        .. currentmodule:: source_inspect
 
         .. autosummary::
            :toctree: _generate
@@ -23,17 +92,31 @@ PYBIND11_MODULE(cmake_example, m) {
            subtract
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+    py::class_<SymLineInfo>(m, "SymLineInfo")
+    .def(py::init<>())
+    .def_readwrite("FileName", &SymLineInfo::FileName)
+    .def_readwrite("LineNumber", &SymLineInfo::LineNumber)
+    .def("__repr__", [](const SymLineInfo &a) {
+        std::wostringstream oss;
+        oss << "<SymLineInfo " <<  a.FileName << L":" << a.LineNumber << L">";
+        return oss.str();
+        });
 
-        Some other explanation about the add function.
-    )pbdoc");
+    py::class_<Inspect>(m, "Inspect")
+    .def(py::init<DWORD>())
+    .def("getLineFromSymName", &Inspect::GetLineFromSymName);
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
+    // m.def("add", &add, R"pbdoc(
+    //     Add two numbers
 
-        Some other explanation about the subtract function.
-    )pbdoc");
+    //     Some other explanation about the add function.
+    // )pbdoc");
+
+    // m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+    //     Subtract two numbers
+
+    //     Some other explanation about the subtract function.
+    // )pbdoc");
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
